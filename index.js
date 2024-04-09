33fdc7690fba5e9ceae4da4f2426e0e1b56f0fbe
Ones <minhquang2002nt@gmail.com>
2024-04-09 21:38:27 +0700

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cartRouter = require('./api/routers/cart.router');
 const adminRouter = require('./api/routers/admin.router');
 const addressVnRouter = require('./api/routers/addres.vn.router');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/fashion_book_db');
+mongoose.connect('mongodb+srv://Quang:Quang@cluster0.pozfjut.mongodb.net/');
 const address = require('./api/models/address.vn.model');
 const test = () => {
     Object.keys(data).forEach( function(k){
